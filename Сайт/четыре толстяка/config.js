a80bca55f5cf23a64e0a1e4a2bc777804681952b
Денис Одноуров <denis.odnourov@mail.ru>
2021-11-26 22:26:06 +0600

    created

--- Сайт/четыре толстяка/config.js
+++ Сайт/четыре толстяка/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'cakes'
+    user: 'p70162fi',
+    password: 'oGM6GU52',
+    database: 'p70162fi_cakes'
 }
